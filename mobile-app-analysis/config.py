441c60f6e66a34f3f03962b426761cbe2d5af807
Sasi Kumar <sasik2159@gmail.com>
2026-07-15 09:24:02 +0530

    added mobile_app_folder

--- mobile-app-analysis/config.py
+++ mobile-app-analysis/config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv()
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Sasi@2308",
-    "database": "mobile_app_analysis"
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
 }
